e9bc647ce95c29e2db91e1636a191f89bff1426a
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-31 16:39:34 -0600

    working on userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,10 +11,17 @@ module.exports = {
   //Get a single user by ID
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      .select('-__v')
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with that ID' })
+          ? res.status(404).json({ message: 'No user found with that ID' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
@@ -26,4 +33,8 @@ module.exports = {
       .then((newUser) => res.json(newUser))
       .catch((err) => res.status(500).json(err));
   },
+
+  //updateUser
+
+  //deleteUser
 };
